6e8927279f6421def07feec3c7bf96b52eedb01b
iffatazeez <126850039+iffatazeez@users.noreply.github.com>
2023-12-21 14:05:03 +1100

    creating the javascript file for the website

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function () {
+    // Run code after the DOM has loaded
+
+    // Example: Change the text color of the header
+    const header = document.querySelector('header');
+    header.style.color = 'red';
+
+    // Add more JavaScript code as needed
+});
